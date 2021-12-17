c5d6b07c62374626038895dfd2c199f24118044b
hi-qiuqiu <18390238313@163.com>
2021-12-17 11:44:44 +0800

    添加javaCode

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # typora-
 typora笔记
 
+java:
 1、慕课网学习：http://www.imooc.com/wiki/javalesson/arrays.html
---
+++ 后端学习/javaProjectCode/javaStudyMethod/src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    int age = 24;
+        String name = "qiuYing";
+        System.out.println("age:" + age + '\n' + "name:" + name);
+    }
+}
